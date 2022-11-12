ba784c998410e3608bf692858ba67ba66b43eed6
noodlefighter <noodlefighter@gmail.com>
2022-11-12 23:36:01 +0800

    embase_macros.h

---
+++ include/embase_macros.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "embase_log.h"
+#include <functional>
+#include <assert.h>
+
+#if EMBASE_DEBUG > 0
+#define EM_ASSERT(cond, msg) if(!(cond)){__log("Assert failed: %s", msg);assert(cond);}
+#else
+#define EM_ASSERT(cond, msg)
+#endif
+
+#define EM_GOTO_IF(cond,too)                        if(cond) goto too;
+#define EM_BREAK_IF(cond)					                  if(cond) break
