035b5e2ff94ca4804f55aa1d6c77669fa9890567
noodlefighter <noodlefighter@gmail.com>
2023-07-06 22:44:13 +0800

    cmake function: print_size_of_target

--- cmake/c-utils.cmake
+++ cmake/c-utils.cmake
@@ -1,3 +1,11 @@
 function(add_map_file target_name)
     target_link_options(${target_name} PRIVATE -Wl,-Map=${target_name}.map)
 endfunction()
+
+function(print_size_of_target TARGET)
+    add_custom_target(${TARGET}_always_display_size
+        ALL COMMAND ${CMAKE_SIZE} "$<TARGET_FILE:${TARGET}>"
+        COMMENT "Target Sizes: "
+        DEPENDS ${TARGET}
+    )
+endfunction()
