8ea08532ba3b44cc587fd0d6aa048b4a9a6c71a7
noodlefighter <noodlefighter@gmail.com>
2023-07-07 15:38:47 +0800

    cmake函数 generate_bin_file(), generate_fw_files()

--- cmake/c-utils.cmake
+++ cmake/c-utils.cmake
@@ -1,11 +1,30 @@
-function(add_map_file target_name)
-    target_link_options(${target_name} PRIVATE -Wl,-Map=${target_name}.map)
+# fixme: 目前仅能在build的根目录生成map文件
+function(add_map_file TARGET)
+    target_link_options(${TARGET} PRIVATE -Wl,-Map=${TARGET}.map)
 endfunction()
 
 function(print_size_of_target TARGET)
     add_custom_target(${TARGET}_always_display_size
-        ALL COMMAND ${CMAKE_SIZE} "$<TARGET_FILE:${TARGET}>"
+        ALL
+        COMMAND ${CMAKE_SIZE} --format=berkeley "$<TARGET_FILE:${TARGET}>"
         COMMENT "Target Sizes: "
         DEPENDS ${TARGET}
     )
+endfunction()
+
+function(generate_bin_file TARGET)
+    add_custom_target(${TARGET}_always_generate_bin_file
+        ALL
+        COMMAND ${CMAKE_OBJCOPY} -Oihex "$<TARGET_FILE:${TARGET}>" "${TARGET}.hex"
+        COMMAND ${CMAKE_OBJCOPY} -Obinary "$<TARGET_FILE:${TARGET}>" "${TARGET}.bin"
+        COMMAND ${CMAKE_OBJDUMP} --all-headers --demangle --disassemble "$<TARGET_FILE:${TARGET}>" > "${TARGET}.lst"
+        COMMENT "Generate Binary File: "
+        DEPENDS ${TARGET}
+    )
+endfunction()
+
+function(generate_fw_files TARGET)
+    generate_bin_file(${TARGET})
+    print_size_of_target(${TARGET})
+    add_map_file(${TARGET})
 endfunction()
